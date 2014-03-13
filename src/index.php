d7dab2405fcc0f03dec4284d67e0b8541445a88e
blzK <bpertev@gmail.com>
2014-03-13 14:01:58 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -3,8 +3,7 @@
 include 'header.php';
 ?>
 
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mi metus, euismod at fringilla ut, volutpat at libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam eget metus et neque ultricies porta id eu augue. Sed sit amet purus faucibus, convallis nulla eget, pretium nisl. Suspendisse tortor ante, venenatis sed ultricies eu, tristique sit amet felis. Etiam dolor neque, vehicula ut velit vel, pellentesque egestas justo. Quisque vulputate adipiscing leo. Vestibulum ut ipsum tristique, cursus ligula eget, suscipit magna. Pellentesque diam lectus, ultricies quis mi eget, commodo ultrices lorem. Suspendisse sed sapien arcu. Donec vitae erat a justo tincidunt fermentum in sed nibh
-
+Connexion 
 <?php
 include 'footer.php';
 ?>
